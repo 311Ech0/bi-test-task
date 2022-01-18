9eaf157667cdf856b8128926311d19f994daeb64
Dmitriy <311kz@outlook.com>
2022-01-18 10:03:06 +0600

    download

---
+++ app/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	// Получаем все доступные сетевые интерфейсы
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, interf := range interfaces {
+		// Список адресов для каждого сетевого интерфейса
+		addrs, err := interf.Addrs()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(w, "Сетевой интерфейс: %s\n", interf.Name)
+
+		for _, add := range addrs {
+			if ip, ok := add.(*net.IPNet); ok {
+				fmt.Fprintf(w, "\tIP: %v\n", ip)
+			}
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8080", nil)
+}
